fa1b14286b0f609b7e9459c392483854c34ec3d6
Eric Booker <eric90booker@gmail.com>
2019-07-11 16:41:54 -0500

    Revert "Added Policy class file."
    This reverts commit 5ddc83ffc725734e94a6f65fda61eb06d8ff37c2.

--- Insurance_Website/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Insurance_Website.Global" Language="C#" %>
---
+++ Website/models/Accounts.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace Website.models
+//{
+//    public class Accounts
+//    {
+//    }
+
+//    public void CreateAccount(Accounts account)
+//    {
+//        Accounts acc = new Accounts();
+//        acc.name = textBox1.Text;
+
+//        CRMConnect conn = new CRMConnect();
+//        conn.CreateAccount(acc);
+//    }
+//}
