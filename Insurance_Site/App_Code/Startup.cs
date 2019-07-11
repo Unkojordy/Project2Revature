5ddc83ffc725734e94a6f65fda61eb06d8ff37c2
Eric Booker <eric90booker@gmail.com>
2019-07-11 15:53:18 -0500

    Added Policy class file. Finished web form for policy(still need to change country to option set) Created Folder for classes to hold information from customer input

---
+++ Insurance_Site/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Insurance_Site.Startup))]
+namespace Insurance_Site
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Insurance_Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Insurance_Website.Global" Language="C#" %>
