03556fd1ed14c499c54d97959d566d1f1c7e9e9c
Eric Booker <eric90booker@gmail.com>
2019-07-13 13:18:12 -0500

    ExchangeRateWorkFlow 1.0.0.0

--- ExchangeRateWorkFlow/UpdateCurrency.cs
+++ ExchangeRateWorkFlow/UpdateCurrency.cs
@@ -27,7 +27,8 @@ namespace ExchangeRateWorkFlow
 
             var canadianRecord = new Entity("transactioncurrency", new Guid("9e6d3598-5f8f-e911-a9c1-000d3a33afd1"));
 
-            canadianRecord.Attributes.Add("exchangerate", )
+            canadianRecord.Attributes.Add("exchangerate", exchangeRate.Value);
+            Service.Update(canadianRecord);
         }
     }
 }
